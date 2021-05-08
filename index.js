1aa582e59e9693cfcff5fed8b3d2716b3236fae2
Andres Garcia <andresf212000@gmail.com>
2021-05-08 01:21:19 -0400

    basic CRUD methods

--- index.js
+++ index.js
@@ -10,8 +10,22 @@ app.use(
 );
 app.options('*', cors());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/backscratchers', (req, res) => {
+    return res.send('Received a GET HTTP method');
+});
 
-app.listen(process.env.PORT || 3000, function() {
+app.post('/backscratchers', (req, res) => {
+    return res.send('Received a POST HTTP method');
+});
+
+app.put('/backscratchers', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/backscratchers', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+});
+
+app.listen(process.env.PORT || 3000, function () {
     console.log('server running on port 3000', '');
 });
