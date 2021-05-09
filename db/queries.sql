6a88b6469942356af5fd6654f5b5aa6f59ea2b0f
Andres Garcia <andresf212000@gmail.com>
2021-05-09 00:23:03 -0400

    updating sql queries; paper trail

--- db/queries.sql
+++ db/queries.sql
@@ -14,3 +14,5 @@ values ('testing', 'description test', array ['XL'], '$5.00') returning *
 select * from backscratchers
 
 update backscratchers set item_size = array ['S'] where id = 1 returning *
+
+delete from backscratchers where id = 1 returning *
